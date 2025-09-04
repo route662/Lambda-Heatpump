f0090c817f945001a4cbfc93b727455d353e36b6
route662 <97343669+route662@users.noreply.github.com>
2025-09-04 13:48:04 +0200

    Update sensor.py
    Fallback

--- custom_components/lambda_heatpump/sensor.py
+++ custom_components/lambda_heatpump/sensor.py
@@ -167,7 +167,18 @@ class ModbusClientManager:
                 _LOGGER.debug(f"Reading registers from {start_register} to {end_register} (count: {count})")
 
                 # Lese die Register im definierten Block
-                result = self.client.read_holding_registers(start_register, count=count, unit=1)
+               # result = self.client.read_holding_registers(start_register, count=count, unit=1)
+
+
+                try:
+                    result = self.client.read_holding_registers(start_register, count=count, unit=1)
+                except TypeError:
+                    result = self.client.read_holding_registers(start_register, count=count, slave=1)
+
+
+
+
+                
                 if result.isError():
                     _LOGGER.error(f"Error reading registers from {start_register} to {end_register}: {result}")
                     continue
